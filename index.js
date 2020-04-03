8f08bd3a7f8acf0b5c716d8ff5c6f2c97cca314e
Jusan Magno Pinto da Silva <jusanmagno@gmail.com>
2020-04-03 18:03:09 -0300

    base do projeto

--- index.js
+++ index.js
@@ -1,107 +1,117 @@
 var express = require('express');
 var app = express();
-var storange = require('./storanger/data.json');
-const bodyParser = require('body-parser')
-const expressLayouts = require('express-ejs-layouts')
+const storange = require('./storanger/data.json');
+const fs = require('fs');
+const bodyParser = require('body-parser');
+const expressLayouts = require('express-ejs-layouts');
 
 app.set('view engine', 'ejs')     // Setamos que nossa engine será o ejs
 app.use(expressLayouts)           // Definimos que vamos utilizar o express-ejs-layouts na nossa aplicação
 //Rota inicial
 app.get('/', (req, res) => {
-  res.render(__dirname + '/views/home', {params : storange});
+  param = storange.reduce((init, city) => {
+    init.push(city.to)
+    init.push(city.from)
+
+    return init
+  }, [])
+
+  params = param.filter((city, i) => {
+    return param.indexOf(city) === i;
+  })
+  res.render(__dirname + '/views/home', { params , sucess: false});
 });
 
 //Rota para calculo de menor preço
 app.get('/quote/:to/:from', (req, res) => {
-  res.send(findRoute(req.params.to, req.params.from))
+  res.send(findRoute(req.params.to.toString(), req.params.from.toString()))
 });
 
+//registrar novas rotas
 app.post('/register', (req, res) => {
+  let param = {
+    from: req.params.from,
+    to: req.params.to,
+    price: req.params.price
+  }
 
+  let data = JSON.stringify(param, null, 2);
+  if(fs.writeFileSync(storange, data)){
+    res.redirect('/', {sucess: "arquivo salvo com sucesso"})
+  }
+  
 });
 
 app.listen(3000, function () {
   console.log('Servidor funcinando na porta 3000!');
 });
 
-var somaPrice = (x) => {
-  function param(y, z) {
-    return y + z;
-  }
-
-}
-
 var findRoute = (from, to) => {
-  var min = 0;
-  var oneDistine;
-  var onePrice;
-  var twoDistine = [];
-  var twoPrice;
-  var threeDistine = [];
-  var threePrice;
-  var arrayTo = storange.filter(function (value) {
-    let obj = Object.values(value)
-    if (obj.indexOf("GRU") > -1) {
-      return value
-    }
-  })
-  var arrayFrom = storange.filter(function (value) {
-    let obj = Object.values(value)
-    if (obj.indexOf("CDG") > -1) {
-      return value
-    }
-  })
-  arrayTo.forEach(elTo => {
-    arrayFrom.forEach(elfrom => {
-      if (elTo.to === elfrom.to && elTo.from === elfrom.from) {
-        oneDistine = elTo
+  //todos os destinos selecionado igual ao destino
+  const onlyTo = storange.filter(travel => travel.to === to || travel.from === to)
+  //Todas as origens que é igual a origem selecionada
+  const onlyFrom = storange.filter(travel => travel.from === from || travel.to === from)
+
+
+  let oneDistine = []
+  onlyTo.filter((to) => {
+    onlyFrom.filter((from) => {
+      if (to.from == from.from && to.to === from.to) {
+        oneDistine.push(to);
       }
-      if (elTo.from != "GRU" && elfrom.to != "CDG" && elTo.from === elfrom.to) {
-        twoDistine.push(elTo, elfrom)
-      }
-      if (elTo.to != "GRU" && elfrom.from != "CDG" && elTo.to === elfrom.from) {
-        threeDistine.push(elTo, elfrom)
-      }
-    });
+    })
+    return oneDistine;
   });
 
-  if (!isEmpty(oneDistine)) {
-    onePrice = Object.keys(oneDistine).indexOf(1) > -1 ? oneDistine.reduce((total, valor) => total + valor.price, 0) : oneDistine.price;
-    min = onePrice
-  }
-  if (!isEmpty(twoDistine)) {
-    twoPrice = Object.keys(twoDistine).indexOf(1) ? twoDistine.reduce((total, valor) => total + valor.price, 0) : twoDistine.price;
-    if (onePrice && onePrice > twoPrice) {
-      min = twoPrice
-    } else {
-      min = onePrice
+
+//primeiro nivel
+// se A.destino === to && 
+let firstLevel = []
+onlyTo.filter(travel => {
+  onlyFrom.forEach(el => {
+    if(travel.to === to && (travel.from === el.from || travel.from === el.to) 
+    || travel.from === to && (travel.to === el.from || travel.to === el.to))
+    {
+      firstLevel.push(travel, el)
     }
-  }
-  if (!isEmpty(threeDistine)) {
-    threePrice = Object.keys(threeDistine).indexOf(1) ? threeDistine.reduce((total, valor) => total + valor.price, 0) : threeDistine.price;
-    if (min > threePrice || min === 0) {
-      min = threePrice
+    return firstLevel
+  });
+})
+
+
+
+
+
+
+  const paralelomach = onlyTo.reduce((init, to) => {
+    onlyFrom.filter((from) => {
+      if (to.from == from.to || to.to === from.from) {
+        init.push(from, to)
+      }
+    })
+    return init
+  }, [])
+
+  const twoDistine = firstLevel.reduce((init, travel) => {
+
+    if (!(travel.from === from && travel.to === to)) {
+      if (travel.from === from || travel.to == to)
+        init.push(travel)
     }
+
+    return init
+  }, [])
+
+return twoDistine;
+  const someTwo = twoDistine.reduce((init, travel) => {
+    return init + travel.price;
+  }, 0)
+
+  if (someTwo > oneDistine.price) {
+    return oneDistine;
+  } else {
+    return twoDistine;
   }
+};
 
 
-  if(onePrice == min){
-    return oneDistine
-  }
-
-  if(twoPrice == min){
-    return twoDistine
-  }
-
-  if(threePrice == min){
-    return threeDistine;
-  }
-
-  console.log(min);
-  // return {"preco1": onePrice, "preco2": twoPrice, "preco3": threePrice};
-  return { "oneD": oneDistine, "twoD": twoDistine, "threeD": threeDistine };
-}
-
-function isEmpty(obj) {
-  return Object.keys(obj).length === 0;
-}
